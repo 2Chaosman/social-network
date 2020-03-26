9dea5d8bb46b3bfd5c425bd2ffadfbf7d25e401e
Сергей Мухин <chaosman@MacBook-Air-Sergej.local>
2020-03-26 19:41:28 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './App.css';
-import Header from "./Components/Header";
-import Navbar from "./Components/Navbar";
-import Profile from "./Components/Profile";
-
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import Profile from "./Components/Profile/Profile";
+import Dialogues from "./Components/Dialogues/Dialogues";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header />
-            <Navbar />
-            <Profile />
-        </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogues' component={Dialogues}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
